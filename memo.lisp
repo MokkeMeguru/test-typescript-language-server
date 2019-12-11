4b71bf8faf7e221a890ffeadf26e4040003be8a5
MokkeMeguru <meguru.mokke@gmail.com>
2019-12-11 15:40:24 +0900

    add loggin option

--- memo.lisp
+++ memo.lisp
@@ -1,6 +1,6 @@
 (lsp-register-client
  (make-lsp-client :new-connection
-                  (lsp-tramp-connection (list "typescript-language-server" "--stdio"))
+                  (lsp-tramp-connection (list "typescript-language-server" "--stdio" "--log-level 4" " --tsserver-log-file /tmp/tslog.txt"))
                   :major-modes '(typescript-mode)
                   :remote? t
                   :server-id 'typescript-remote))
