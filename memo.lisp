a5847eaff0f682f4ff79fd60213f069bbefbe72b
MokkeMeguru <meguru.mokke@gmail.com>
2019-12-11 17:12:07 +0900

    add new snippet in memo.lisp

--- memo.lisp
+++ memo.lisp
@@ -1,9 +1,42 @@
-(lsp-register-client
- (make-lsp-client :new-connection
-                  (lsp-tramp-connection (list "typescript-language-server" "--stdio" "--log-level 4" " --tsserver-log-file /tmp/tslog.txt"))
-                  :major-modes '(typescript-mode)
-                  :remote? t
-                  :server-id 'typescript-remote))
+;; how to run typescript-language-server with docker project
+;; 1. run in eshell
+(add-to-list 'load-path "~/.emacs.d/lsp-docker/")
+(require 'lsp-docker)
+
+;; 2. run in multi-term
+;;;;;;;;;
+;; docker-compose run workspace
+;; docker ps
+;; =>
+;; ac2736529d12        test-ts-lsp_workspace   "docker-entrypoint.s…"   31 seconds ago      Up 28 seconds                                  test-ts-lsp_workspace_run_5a93d2877899
+;;;;;;;;;
+
+;; 3. run in eshell
+
+(lsp-docker-register-client
+ :server-id 'ts-ls
+ :priority 1
+ :docker-server-id 'typescript-docker
+ :docker-image-id "ac2736529d12"
+ :docker-container-name "test-ts-lsp_workspace_run_5a93d2877899"
+ :server-command "typescript-language-server --stdio --log-level 4 --tsserver-log-file /tmp/tslog.txt"
+ :path-mappings '(("/home/meguru/Github/test-ts-lsp" . "/usr/app"))
+ :launch-server-cmd-fn #'lsp-docker-launch-new-container)
+
 
 ;; for showing lsp log
-(setq lsp-print-io t)
+;; (setq lsp-print-io t)
+
+
+
+
+
+
+;; memo
+
+;; (lsp-register-client
+;;  (make-lsp-client :new-connection
+;;                   (lsp-tramp-connection (list "typescript-language-server" "--stdio" "--log-level 4" " --tsserver-log-file /tmp/tslog.txt"))
+;;                   :major-modes '(typescript-mode)
+;;                   :remote? t
+;;                   :server-id 'ts-ls))
